93ea6b1c196730c92fdc3baa8ab04824a8847fe2
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-07-05 20:52:38 -0300

    feat: Created endpoint ("/home")
    Feature details:
    
    (*) Get all transations given user token and return transation list.

--- src/controllers/transation.controller.js
+++ src/controllers/transation.controller.js
@@ -1,8 +1,41 @@
-import { transationSchema } from "../schemas/transation.schema.js";
 import dayjs from "dayjs";
 import db from "../database/db.js";
 
-export async function getTransations(req, res) {
+import { transationSchema } from "../schemas/transation.schema.js";
+
+export async function getTransactions(req, res) {
+
+    const { authorization } = req.headers;
+
+    const token = authorization?.replace("Bearer ", "");
+
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    try {
+
+        const sessionDB = await db.collection("sessions").findOne({ token });
+
+        if (!sessionDB) {
+            return res.sendStatus(401);
+        }
+
+        const transationDb = await db.collection("transations").findOne({ userId: sessionDB.userId });
+
+        const transationsList = {
+            username: transationDb.name,
+            transations: (transationDb.transations).reverse()
+        };
+
+        res.send(transationsList);
+
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export async function newTransation(req, res) {
     
     const { value, description } = req.body;
     const { type } = req.params;
@@ -24,7 +57,7 @@ export async function getTransations(req, res) {
     try {
 
         const sessionDB = await db.collection("sessions").findOne({ token });
-      
+
         if (!sessionDB) {
             return res.sendStatus(401);
         }
@@ -33,11 +66,13 @@ export async function getTransations(req, res) {
 
         if (!userDB) {
             return res.sendStatus(401);
-        }       
+        }
 
         const transationInfo = {
-            value: value,
+
+            value: parseFloat(value.toFixed(2)),
             description: description,
+            type: type,
             date: dayjs().locale('pt-br').format('DD/MM')
         }
 
@@ -45,7 +80,11 @@ export async function getTransations(req, res) {
 
         if (!transationsDB) {
 
-            await db.collection("transations").insertOne({ userId: userDB._id, transations: [transationInfo] });
+            await db.collection("transations").insertOne({ 
+                userId: userDB._id,
+                name: userDB.name,
+                transations: [transationInfo]
+            });
 
         } else {
 
--- src/routers/transation.route.js
+++ src/routers/transation.route.js
@@ -1,8 +1,9 @@
-import { getTransations } from "../controllers/transation.controller.js";
+import { getTransactions, newTransation } from "../controllers/transation.controller.js";
 import { Router } from "express";
 
 const transationRouter = Router();
 
-transationRouter.post("/nova-transacao/:type", getTransations);
+transationRouter.get("/home", getTransactions);
+transationRouter.post("/nova-transacao/:type", newTransation);
 
 export default transationRouter;
