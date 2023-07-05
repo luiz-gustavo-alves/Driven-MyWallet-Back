739b35ec4108c3c3ba68ddd357d8378225f030d3
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-07-05 12:03:40 -0300

    Created connectToDatabase function
    Details:
    - App.js is now able to connect to MongoDB database.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { connectToDatabase } from "./database/db.js";
 
-dotenv.config();
+/* Import Routers */
+import authRouter from "./routers/auth.route.js";
+
+dotenv.config()
+
+connectToDatabase();
 
 /* API configuration*/
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+/* Endpoints */
+app.use([authRouter]);
 
 app.listen(process.env.PORT);
--- src/database/db.js
+++ src/database/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
-async () => {
+export async function connectToDatabase() {
     try {
         await mongoClient.connect();
         console.log("Conectado com banco de dados.");
